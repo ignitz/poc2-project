364bc47ea63db1bda486a3e7eb58b40709046ab3
Yuri Niitsuma <ignitzhjfk@gmail.com>
2020-10-26 03:16:42 -0300

    Fix dev folder

--- copy_jars_to_lambda-emr-run-deltalake.sh
+++ copy_jars_to_lambda-emr-run-deltalake.sh
@@ -4,5 +4,5 @@ mkdir -p infra/modules/lambdas/emr-run-deltalake/build/dev \
     && mkdir -p infra/modules/lambdas/emr-run-deltalake/build/prd \
     && mkdir -p infra/modules/lambdas/emr-run-deltalake/build/hml \
     && cp -v deltalake-processing/target/scala-2.12/deltalake-processing-assembly-1.0.jar \
-       infra/modules/lambdas/emr-run-deltalake/build/deltalake-processing-assembly-1.0.jar \
+       infra/modules/lambdas/emr-run-deltalake/build/dev/deltalake-processing-assembly-1.0.jar \
     && echo "Copying jars done."
