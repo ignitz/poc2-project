a44f99ad09614c5156497b281eb35526eeda2b5a
Yuri Niitsuma <ignitzhjfk@gmail.com>
2020-10-26 03:02:41 -0300

    Fix typos

--- build_all.sh
+++ build_all.sh
@@ -3,7 +3,7 @@
 # Must have sbt assembly and terraform
 
 ./copy_zip_to_build_stacks.sh \
-&& (cd deltalake-processing && sbt assembly) \
-&& ./copy_jars_to_lambda-emr-run-deltalake.sh
+&& (cd deltalake-processing && docker run -it --rm -v sbt_ivy_data:/root/.ivy2 -v sbt_data:/root/.sbt -v $PWD:/app -w /app mozilla/sbt sbt assembly) \
+&& ./copy_jars_to_lambda-emr-run-deltalake.sh \
 && (cd infra && make) \
 && echo "Build All DONE."
